5c8256623a5e6c7dfd964a20d3249ec4053d0f2f
= <maria.louisa.castro@gmail.com>
2022-01-17 00:48:22 +0000

    Create first category object and add onlick to buttons in HTML
    Realtes #3

---
+++ index.js
@@ -0,0 +1,26 @@
+const colours = [
+  {
+    english: "red",
+    spanish: "rojo",
+    pronounciaion: "ro-ho",
+  },
+  {
+    english: "blue",
+    spanish: "azul",
+    pronounciaion: "ah-zul",
+  },
+  {
+    english: "yellow",
+    spanish: "amarillo",
+    pronounciaion: "ah-ma-ri-yo",
+  },
+  {
+    english: "white",
+    spanish: "blanco",
+    pronounciaion: "blan-ko",
+  },
+];
+
+function selectCat(e) {
+  console.log(e);
+}
